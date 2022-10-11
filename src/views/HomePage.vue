9d9108322b9a906d550452201596dbb014c473a1
hchavezj <hchavezj@gmail.com>
2022-10-10 23:39:25 -0500

    Category Call

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,4 +1,12 @@
-<script setup></script>
+<script setup>
+import { onMounted, ref } from 'vue'
+import useAPI from '@/composables/useAPI'
+const api = useAPI()
+const categories = ref([])
+onMounted(async () => {
+  categories.value = await api.getCategories()
+})
+</script>
 
 <template>
   <div class="brand">
@@ -7,8 +15,13 @@
     <img class="logo" src="logo.svg" alt="logo" />
   </div>
   <div class="categories">
-    <RouterLink v-for="n in 24" :key="n" :to="`/question/category/${n}`" class="category"
-      >Categories - {{ n }}</RouterLink
+    <RouterLink
+      v-for="category in categories"
+      :key="category.id"
+      :to="`/question/category/${category.id}`"
+      class="category"
+    >
+      {{ category.name }}</RouterLink
     >
   </div>
 </template>
@@ -29,7 +42,7 @@
   @apply grid flex-grow grid-cols-4 gap-4;
 
   & .category {
-    @apply flex h-32 w-40 items-center justify-center rounded-lg border-4 border-slate-900 py-4 text-center font-bold uppercase transition-colors duration-300;
+    @apply flex h-32 items-center justify-center rounded-lg border-4 border-slate-900 p-4 text-center font-bold uppercase transition-colors duration-300;
 
     &:hover {
       @apply cursor-pointer bg-slate-900;
