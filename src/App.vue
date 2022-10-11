2edf4514fb5b793685e0bff6cda2ab846e067c2f
hchavezj <hchavezj@gmail.com>
2022-10-10 22:58:32 -0500

    Mane Page Layout

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,18 @@
 <script setup></script>
 
 <template>
-  <RouterView />
+  <main>
+    <div class="card">
+      <RouterView />
+    </div>
+  </main>
 </template>
+
+<style lang="postcss" scoped>
+main {
+  @apply flex h-screen items-center justify-center bg-gradient-to-b from-gray-900 to-gray-600;
+  & .card {
+    @apply flex h-5/6 min-h-fit w-11/12 flex-col items-center gap-16 overflow-auto rounded-lg bg-slate-600 p-8 text-slate-200 shadow-lg;
+  }
+}
+</style>
--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,11 +1,39 @@
 <script setup></script>
 
 <template>
-  <div class="nav">
-    <RouterLink to="/">Home</RouterLink> |
-    <RouterLink to="/question/category/x">Question</RouterLink>
+  <div class="brand">
+    <img class="logo" src="logo.svg" alt="logo" />
+    <h1 class="title">Triviantastic</h1>
+    <img class="logo" src="logo.svg" alt="logo" />
   </div>
-  <div>
-    <h1 class="text-6xl font-thin">I'm The Home Page</h1>
+  <div class="categories">
+    <RouterLink v-for="n in 24" :key="n" :to="`/question/category/${n}`" class="category"
+      >Categories - {{ n }}</RouterLink
+    >
   </div>
 </template>
+
+<style lang="postcss" scoped>
+.brand {
+  @apply flex items-center justify-center gap-4;
+
+  & .logo {
+    @apply h-16 w-16;
+  }
+  & .title {
+    @apply text-6xl font-thin uppercase tracking-widest text-slate-200;
+  }
+}
+
+.categories {
+  @apply grid flex-grow grid-cols-4 gap-4;
+
+  & .category {
+    @apply flex h-32 w-40 items-center justify-center rounded-lg border-4 border-slate-900 py-4 text-center font-bold uppercase transition-colors duration-300;
+
+    &:hover {
+      @apply cursor-pointer bg-slate-900;
+    }
+  }
+}
+</style>
